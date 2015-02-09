f1f1c3ee6c4b832b1e456ab6dbd459c8d84828ee
Luke Engvall <engvall@colorado.edu>
2015-02-09 15:44:03 -0700

    Commit with code showing optimal convergence. Still need more unit testing and documentation before initial beta release is ready.

--- showNodes.m
+++
@@ -1,9 +0,0 @@
-function [] = showNodes(node)
-hold on
-x = node(:,1);
-y = node(:,2);
-
-scatter(x,y);
-a = [1:10]'; b = num2str(a); c = cellstr(b);
-dx = 0.02; dy = 0.02; % displacement so the text does not overlay the data points
-text(x+dx, y+dy, c);
